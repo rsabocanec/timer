2d053d17d455620804729144e2c006a07a0ae20f
Robert Sabočanec <r.sabocanec@altpro.hr>
2025-07-13 19:23:03 +0200

    Switched to file descriptor's timers

--- main.cpp
+++ main.cpp
@@ -1,236 +1,142 @@
-#include <csignal>
-#include <ctime>
-#include <cstring>
-
-/*
-int timer_create(clockid_t clockid,
-                 struct sigevent *_Nullable restrict sevp,
-                 timer_t *restrict timerid);
-*/
-#include <future>
-#include <thread>
-#include <chrono>
+//
+// Created by r.sabocanec on 13.7.2025..
+//
+#include <source_location>
 #include <iostream>
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <netinet/in.h>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
 
+#include <sys/timerfd.h>
+#include <sys/types.h>
+
+#include <err.h>
 #include <unistd.h>
 
-#define _USING_SOCKET
-
 namespace {
-#if 0
+    int32_t report_error(
+        const std::string_view prefix = std::source_location::current().function_name(),
+        const int32_t error_num = errno) noexcept {
+
+        std::cerr   << prefix << " returned error " << error_num
+                    << '[' << ::strerror(error_num) << ']' << std::endl;
+
+        return error_num;
+    }
+
     class timer {
-        int32_t init() noexcept {
-            //auto result = ::timer_create(CLOCK_MONOTONIC, )
+        int descriptor_{-1};
+
+        public:
+        timer() noexcept {
+            [[maybe_unused]] auto const result = open();
+        }
+
+        explicit timer(uint32_t seconds, uint32_t nanoseconds = 0ul) noexcept {
+            if (open() == 0) {
+                [[maybe_unused]] auto const result = arm(seconds, nanoseconds);
+            }
+        }
+
+        timer(const timer&) = delete;
+        timer(timer&&) = default;
+
+        timer& operator=(const timer&) = delete;
+        timer& operator=(timer&&) = default;
+
+        ~timer() {
+            if (descriptor_ != -1) {
+                [[maybe_unused]] auto const result = ::close(descriptor_);
+            }
+
+            descriptor_ = -1;
+        };
+
+        int32_t arm(uint32_t seconds, uint32_t nanoseconds = 0ul) noexcept {
+            if (descriptor_ != -1) {
+                struct timespec now{};
+                if (::clock_gettime(CLOCK_MONOTONIC, &now) != 0) {
+                    return report_error("clock_gettime()");
+                }
+
+                const struct itimerspec period = {
+                    .it_interval = {.tv_sec = seconds, .tv_nsec = nanoseconds},
+                    .it_value = {.tv_sec = now.tv_sec + seconds, .tv_nsec = now.tv_nsec}
+                };
+
+                if (::timerfd_settime(descriptor_, TFD_TIMER_ABSTIME, &period, nullptr) == -1) {
+                    return report_error("arm(), timerfd_settime()");
+                }
+
+                return 0;
+            }
+
             return -1;
         }
+
+        int32_t disarm() noexcept {
+            constexpr struct itimerspec period = {};
+
+            if (::timerfd_settime(descriptor_, TFD_TIMER_ABSTIME, &period, nullptr) == -1) {
+                return report_error("disarm(), timerfd_settime()");
+            }
+
+            return 0;
+        }
+
+        int32_t wait() noexcept {
+            if (descriptor_ != -1) {
+                uint64_t exp{};
+                auto const result = ::read(descriptor_, &exp, sizeof(exp));
+                if (result == -1) {
+                    return report_error("wait(), read()");
+                }
+                else if (result != sizeof(exp)) {
+                    std::cerr << "Read timer returned wrong length: " << result << std::endl;
+                }
+                else {
+                    std::cout << "Timer expired " << exp << std::endl;
+                }
+
+                return 0;
+            }
+
+            return -1;
+        }
+
+    private:
+        int32_t open() noexcept {
+            auto const result = ::timerfd_create(CLOCK_MONOTONIC, 0);
+            if (result == -1) {
+                return report_error();
+            }
+
+            descriptor_ = result;
+            return 0;
+        }
+
+        int32_t close() noexcept {
+            if (descriptor_ != -1) {
+                auto const result = ::close(descriptor_);
+                if (result == -1) {
+                    return errno;
+                }
+            }
+
+            descriptor_ = -1;
+            return 0;
+        }
     };
-#endif
-
-    struct timer_signal_data {
-        timer_t timer_id_;
-        int socket_;
-    };
-
-    constexpr std::string_view notify_address{"/tmp/timer_notify"};
-
-    void timer_thread_handler(sigval value) {
-        std::cout << "Timer elapsed!\n";
-        // Do something with the si pointer
-        auto *tsd = static_cast<timer_signal_data*>(value.sival_ptr);
-        constexpr auto eof = static_cast<unsigned char>(0xff);
-#ifdef _USING_SOCKET
-        [[maybe_unused]] auto result = ::write(tsd->socket_, &eof, 1);
-#endif
-    }
-
-    void timer_signal_handler(int sig, siginfo_t *si, void *uc) {
-        std::cout << "Signal received!\n";
-        // Do something with the si pointer
-        auto *tsd = static_cast<timer_signal_data*>(si->si_value.sival_ptr);
-        constexpr auto eof = static_cast<unsigned char>(0xff);
-#ifdef _USING_SOCKET
-        [[maybe_unused]] auto result = ::write(tsd->socket_, &eof, 1);
-#endif
-        ::signal(sig, SIG_IGN);
-    }
 }
 
-int main(int argc, char** argv) {
-    struct sigaction sa {
-        .sa_flags = SA_SIGINFO
-    };
-
-    sa.sa_sigaction = timer_signal_handler;
-
-    ::sigemptyset((&sa.sa_mask));
-
-    auto timer_signal{SIGRTMIN};
-
-    if (::sigaction(timer_signal, &sa, nullptr) == -1) {
-        std::cerr << "sigaction() failed with error " << errno << std::endl;
-        return EXIT_FAILURE;
+auto main(int argc, char** argv) -> int {
+    timer t(1);
+    for (auto i = 0; i < 10; ++i) {
+        [[maybe_unused]] auto const result = t.wait();
     }
-
-    // Block the timer signal temporary
-    sigset_t mask{};
-    ::sigemptyset(&mask);
-    ::sigaddset(&mask, timer_signal);
-    if (::sigprocmask(SIG_SETMASK, &mask, nullptr) == -1) {
-        std::cerr << "sigprocmask() failed with error " << errno << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    timer_signal_data tsd {
-        .timer_id_ = nullptr,
-        .socket_ = -1
-    };
-
-#ifdef _USING_SOCKET
-    tsd.socket_ = ::socket(PF_UNIX, SOCK_STREAM, IPPROTO_IP);
-    if (tsd.socket_ == -1) {
-        std::cerr << "socket() failed with error " << errno << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    struct sockaddr_un sock_address{.sun_family = PF_UNIX};
-    ::strncpy(sock_address.sun_path, notify_address.data(), notify_address.length());
-
-    auto listening_socket = ::socket(PF_UNIX, SOCK_STREAM, IPPROTO_IP);
-    if (listening_socket == -1) {
-        std::cerr << "listening socket() failed with error " << errno << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    if (::bind(listening_socket,
-            static_cast<const sockaddr*>(static_cast<const void *>(&sock_address)),
-        sizeof(sock_address)) == -1) {
-        std::cerr << "bind() failed with error " << errno << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    if (::listen(listening_socket, 1) == -1) {
-        std::cerr << "listen() failed with error " << errno << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    std::cout << "Starting listener thread!\n";
-
-    std::promise<void> promise;
-    std::future<void> future = promise.get_future();
-
-    auto listener = std::thread([](int sock, std::promise<void>&& p) {
-        auto read_sock = ::accept(sock, nullptr, nullptr);
-        if (read_sock == -1) {
-            std::cerr << "accept() failed with error " << errno << std::endl;
-        }
-        else {
-            std::cout << "Connection accepted!\n";
-            unsigned char buffer[256];
-            auto const read_result = ::read(read_sock, buffer, sizeof buffer);
-            if (read_result == -1) {
-                std::cerr << "read() failed with error " << errno << std::endl;
-            }
-            else {
-                std::cout << "Received " << read_result << " bytes!" << std::endl;
-            }
-
-            if (::shutdown(read_sock, SHUT_RD)) {
-                std::cerr << "shutdown() failed with error " << errno << std::endl;
-            }
-
-            ::close(read_sock);
-        }
-
-        p.set_value();
-    }, listening_socket, std::move(promise));
-
-    std::this_thread::yield();
-
-    if (::connect(tsd.socket_,
-            static_cast<const sockaddr*>(static_cast<const void *>(&sock_address)),
-            sizeof(sock_address)) == -1) {
-        std::cerr << "connect() failed with error " << errno << std::endl;
-    }
-
-    std::cout << "Connection established!\n";
-#endif
-    struct sigevent sev {
-        .sigev_value = { .sival_ptr = &tsd },
-        .sigev_signo = timer_signal,
-#if 0
-        .sigev_notify = SIGEV_THREAD,
-        .sigev_notify_function = timer_thread_handler,
-        .sigev_notify_attributes = nullptr
-#else
-        .sigev_notify = SIGEV_SIGNAL
-#endif
-    };
-
-    if (::timer_create(CLOCK_MONOTONIC, &sev, &tsd.timer_id_) == -1) {
-        std::cerr << "timer_create() failed with error " << errno << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    std::cout << "Timer successfully created!\n";
-
-    struct itimerspec its {
-        .it_interval = {
-            .tv_sec = 10,
-            .tv_nsec = 0
-        },
-        .it_value = {
-            .tv_sec = 10,
-            .tv_nsec = 0,
-        }
-    };
-
-    if (::timer_settime(tsd.timer_id_, 0, &its, nullptr) == -1) {
-        std::cerr << "timer_settime() failed with error " << errno << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    std::cout << "Timer started!\n";
-
-    if (::sigprocmask(SIG_UNBLOCK, &mask, nullptr) == -1) {
-        std::cerr << "sigprocmask() failed with error " << errno << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    //std::this_thread::sleep_for(std::chrono::seconds(15));
-
-#ifdef _USING_SOCKET
-    unsigned int counter = 0;
-    while (future.wait_for(std::chrono::seconds(1)) == std::future_status::timeout) {
-        std::cout << ++counter << '\n';
-    }
-#endif
-
-#ifndef _USING_SOCKET
-    if (::sigprocmask(SIG_UNBLOCK, &mask, nullptr) == -1) {
-        std::cerr << "sigprocmask() failed with error " << errno << std::endl;
-        return EXIT_FAILURE;
-    }
-#endif
-
-    if (::timer_delete(tsd.timer_id_) == -1) {
-        std::cerr << "timer_delete() failed with error " << errno << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    ::close(tsd.socket_);
-#ifdef _USING_SOCKET
-    // Wait for the timer to elapse
-    listener.join();
-
-    ::close(listening_socket);
-
-    ::unlink((notify_address.data()));
-#endif
 
     return EXIT_SUCCESS;
 }
